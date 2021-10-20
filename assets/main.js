f34d7a96e732e8e010fc8d4a6e482dc584a5bbc9
Severin Stephan <severin.stephan@gmx.degit config --global user.email>
2021-10-20 14:26:53 +0200

    well

--- assets/main.js
+++ assets/main.js
@@ -19,15 +19,15 @@ let textContent = document.getElementById("abouttext")
 const radioButton = document.getElementById("kurz")
 
 function kurz() {
-    textContent.innerHTML = "<p><b>Severin Stephan</b> macht super tolle Webseiten!</p>"
+    textContent.innerHTML = "<p> Ich mache super tolle Webseiten!</p>"
 }
 
 function mittel() {
-    textContent.innerHTML = "<p><b>Severin Stephan</b> hat eine Weiterbildung zur Web Entwicklerin gemacht und arbeitet jetzt selbständig. Sie ist eine waschechte Berlinerin, lebt in Prenzlauer Berg, liebt das Reisen und die Natur. </p>"
+    textContent.innerHTML = "<p> Ich habe eine Weiterbildung zur Web Entwicklerin gemacht und arbeite jetzt selbständig. Ich bin eine waschechte Berlinerin, lebe in Prenzlauer Berg, liebe das Reisen und die Natur. </p>"
 }
 
 function langeVersion() {
-    textContent.innerHTML = "<p><b>Severin Stephan</b> Liebt die Magie die passiert wenn man all diese Zeichen und Nummer eingibt. Es sieht aus wie ein Chaos aber am Ende kommt eine wunderschöne Webseite dabei raus. 2018 ist Severin zum ersten Mal in die Welt von HTML und CSS eingetaucht und hat stundenlang im Schlafanzug mit Tee und Cracker vor dem Laptop gesessen. Da wusste sie, das ist etwas womit sie ihren Lebensunterhalt verdienen will. Ein Job der Spass macht. Toll! Also hat sie mitten im Lockdown eine Weiterbildung zur Web Entwicklerin gemacht und ist nun fertig. Sie freut sich anderen zu helfen ihre Ideen in das World Wide Web zu laden! </p>"
+    textContent.innerHTML = "<p> Diese Magie die passiert wenn man all diese Zeichen und Nummer eingibt. Es sieht aus wie ein Chaos aber am Ende kommt eine wunderschöne Webseite dabei raus. 2018 bin ich zum ersten Mal in die Welt von HTML und CSS eingetaucht und habe stundenlang im Schlafanzug mit Tee und Cracker vor dem Laptop gesessen. Da wusste ich, das ist etwas womit ich meien Lebensunterhalt verdienen will. Ein Job der Spass macht. Toll! Also habe ich mitten im Lockdown eine Weiterbildung zur Web Entwicklerin gemacht und bin nun fertig. ich freue mich anderen zu helfen ihre Ideen in das World Wide Web zu laden! </p>"
 }
 
 // GALLERY
@@ -36,19 +36,19 @@ const mainPic = document.getElementById("mainpic")
 
 
 function wald() {
-    mainPic.innerHTML = "<img src=assets/img/Portrait4.jpg style= width:60%;display:block;margin:auto;margin-top:15%>"
+    mainPic.innerHTML = "<img src=assets/img/Portrait4NEU.png style= width:60%;display:block;margin:auto;margin-top:15%>"
 }
 
 function bus() {
-    mainPic.innerHTML = "<img src=assets/img/Portrait7.jpg style= width:60%;display:block;margin:auto;margin-top:15%>"
+    mainPic.innerHTML = "<img src=assets/img/ichBus.png style= width:60%;display:block;margin:auto;margin-top:15%>"
 }
 
 function hund() {
-    mainPic.innerHTML = "<img src=assets/img/Portrait8.jpg style= width:60%;display:block;margin:auto;margin-top:15%>"
+    mainPic.innerHTML = "<img src=assets/img/IchPalmen.png style= width:60%;display:block;margin:auto;margin-top:15%>"
 }
 
 function klein() {
-    mainPic.innerHTML = "<img src=assets/img/Portrait12.jpg style= width:60%;display:block;margin:auto;margin-top:15%>"
+    mainPic.innerHTML = "<img src=assets/img/Portrait12NEU.png style= width:60%;display:block;margin:auto;margin-top:15%>"
 }
 
 // PROJECTS
